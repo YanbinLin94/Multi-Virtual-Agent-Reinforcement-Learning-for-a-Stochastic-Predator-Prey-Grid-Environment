c38a49c476c9d15a7d2405bb4384756db71f5b16
Wen-Chung (Andy) Cheng <wcheng3@fau.edu>
2025-05-12 04:01:38 -0400

    remove files that students will generate later on. commented some code blocks

--- plot_fig-eps-STEP-boxplot.py
+++ plot_fig-eps-STEP-boxplot.py
@@ -10,7 +10,7 @@ plt.rcParams["figure.autolayout"] = True
 ax2 = df.plot.box()
 plt.tick_params(labelsize=20)
 labels = ax2.get_xticklabels() + ax2.get_yticklabels()
-[label.set_fontname('Time New Roman') for label in labels]
+# [label.set_fontname('Time New Roman') for label in labels]
 plt.ylabel('Average step per episode',fontsize=22)
 plt.xlabel('Epsilons',fontsize=22)
 plt.savefig("fig4.png", dpi=300)
--- plot_fig-eps0.01--0.8-P4321.py
+++ plot_fig-eps0.01--0.8-P4321.py
@@ -2,17 +2,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-csv_dataM1_eps5 = pd.read_csv('Q_learning_M3-P4321-eps0.01-50runs5000episode.csv',skiprows= lambda x: x>0 and (x-1)%10 !=0)
+# csv_dataM1_eps5 = pd.read_csv('Q_learning_M3-P4321-eps0.01-50runs5000episode.csv',skiprows= lambda x: x>0 and (x-1)%10 !=0)
 csv_dataM1_eps10 = pd.read_csv('Q_learning_M3-P4321-eps0.1-50runs5000episode.csv',skiprows= lambda x: x>0 and (x-1)%10 !=0)
-csv_dataM1_eps30 = pd.read_csv('Q_learning_M3-P4321-eps0.3-50runs5000episode.csv',skiprows= lambda x: x>0 and (x-1)%10 !=0)
-csv_dataM1_eps50 = pd.read_csv('Q_learning_M3-P4321-eps0.6-50runs5000episode.csv',skiprows= lambda x: x>0 and (x-1)%10 !=0)
-csv_dataM1_eps80 = pd.read_csv('Q_learning_M3-P4321-eps0.8-50runs5000episode.csv',skiprows= lambda x: x>0 and (x-1)%10 !=0)
+# csv_dataM1_eps30 = pd.read_csv('Q_learning_M3-P4321-eps0.3-50runs5000episode.csv',skiprows= lambda x: x>0 and (x-1)%10 !=0)
+# csv_dataM1_eps50 = pd.read_csv('Q_learning_M3-P4321-eps0.6-50runs5000episode.csv',skiprows= lambda x: x>0 and (x-1)%10 !=0)
+# csv_dataM1_eps80 = pd.read_csv('Q_learning_M3-P4321-eps0.8-50runs5000episode.csv',skiprows= lambda x: x>0 and (x-1)%10 !=0)
 
-Prob1 = csv_dataM1_eps5.iloc[:,1]
+# Prob1 = csv_dataM1_eps5.iloc[:,1]
 Prob2 = csv_dataM1_eps10.iloc[:,1]
-Prob3 = csv_dataM1_eps30.iloc[:,1]
-Prob4 = csv_dataM1_eps50.iloc[:,1]
-Prob5 = csv_dataM1_eps80.iloc[:,1]
+# Prob3 = csv_dataM1_eps30.iloc[:,1]
+# Prob4 = csv_dataM1_eps50.iloc[:,1]
+# Prob5 = csv_dataM1_eps80.iloc[:,1]
 
 figsize = 6,6
 figure, ax =plt.subplots(figsize=figsize)
@@ -20,22 +20,26 @@ plt.xlim((0,5000))
 plt.ylim(0,0.8)
 x = [y*100 for y in range(51)]
 
-A, = plt.plot(x,Prob1,marker='o',ms=5,mew=1, label ='epsilon=0.01')
+# A, = plt.plot(x,Prob1,marker='o',ms=5,mew=1, label ='epsilon=0.01')
 B, = plt.plot(x,Prob2,marker='*',ms=5,mew=1, label='epsilon=0.1')
-C, = plt.plot(x,Prob3,marker='+',ms=5,mew=1, label='epsilon=0.3')
-D, = plt.plot(x,Prob4,marker='.',ms=5,mew=1, label ='epsilon=0.6')
-E, = plt.plot(x,Prob5,marker='s',ms=5,mew=1, label ='epsilon=0.8')
+# C, = plt.plot(x,Prob3,marker='+',ms=5,mew=1, label='epsilon=0.3')
+# D, = plt.plot(x,Prob4,marker='.',ms=5,mew=1, label ='epsilon=0.6')
+# E, = plt.plot(x,Prob5,marker='s',ms=5,mew=1, label ='epsilon=0.8')
 
 plt.tick_params(labelsize=16)
 labels = ax.get_xticklabels() + ax.get_yticklabels()
-[label.set_fontname('Time New Roman') for label in labels]
+# [label.set_fontname('Time New Roman') for label in labels]
 plt.ylabel('Win probability',fontsize=19)
 plt.xlabel('Episodes',fontsize=20)
-font1 ={'family':'Times New Roman',
-        'weight':'normal',
+# font1 ={'family':'Times New Roman',
+#         'weight':'normal',
+#         'size':20,
+#         }
+font1 ={'weight':'normal',
         'size':20,
         }
-legend = plt.legend(handles=[A,B,C,D,E],prop=font1)
+# legend = plt.legend(handles=[A,B,C,D,E],prop=font1)
+legend = plt.legend(handles=[B],prop=font1)
 plt.savefig("fig1-5000.png", dpi=300)
 plt.show()
 
